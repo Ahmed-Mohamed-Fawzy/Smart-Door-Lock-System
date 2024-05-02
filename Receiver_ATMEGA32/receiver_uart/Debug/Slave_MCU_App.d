316f8a2fb3a95e50476fabe5e848726fc40c8fe1
Ahmed Fawzy <148709604+Ahmed-Mohamed-Fawzy@users.noreply.github.com>
2024-05-02 15:32:08 +0300

    Add files via upload

---
+++ Receiver_ATMEGA32/receiver_uart/Debug/Slave_MCU_App.d
@@ -0,0 +1,12 @@
+Slave_MCU_App.o Slave_MCU_App.o: ../Slave_MCU_App.c ../uart.h \
+  ../std_types.h ../gpio.h ../lcd.h ../dc_motor.h
+
+../uart.h:
+
+../std_types.h:
+
+../gpio.h:
+
+../lcd.h:
+
+../dc_motor.h:
